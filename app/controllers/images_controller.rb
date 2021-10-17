1ae479033771ec5e56fd8646aa9a37745061bca6
mohamed <drakelogan52@gmail.com>
2021-10-17 14:59:24 +0200

    completed image resource

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,6 @@
 class ImagesController < ApplicationController
-    before_action :set_product, only: [:new, :create]
+    before_action :set_product, only: [:new, :create, :confirm, :destroy]
+    before_action :set_image, only: [:confirm, :destroy]
 
     def new
         @image = Image.new
@@ -16,9 +17,22 @@ class ImagesController < ApplicationController
         end
     end
 
+    def confirm
+    end
+
+    def destroy
+        @image.destroy
+        flash[:success] = 'تم حذف الصوره بنجاح.'
+        redirect_to category_product_path(@product.category, @product)
+    end
+
     private
     def set_product
         @product = Product.find(params[:product_id])
+    end
+
+    def set_image
+        @image = Image.find(params[:id])
     end
 
     def image_params
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
   get 'products', to: 'products#list', as: 'products'
   get 'categories/:id/confirm', to: 'categories#confirm', as: 'confirm_category'
+  get 'categories/:category_id/products/:product_id/images/:id/confirm', to: 'images#confirm', as: 'confirm_category_product_image'
   get 'categories/:category_id/sub_categories/:id/confirm', to: 'sub_categories#confirm', as: 'confirm_category_sub_category'
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
